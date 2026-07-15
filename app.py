18d019ebab0e5f72d09f5e4bdfae0b5e649fa909
nadyellysoares <nadyelly.soares@academico.ifpb.edu.br>
2026-07-15 12:39:35 -0300

    NOMES ADICIONADOS

--- app.py
+++ app.py
@@ -9,4 +9,9 @@ equipes.append("Teste")
 
 equipes.append("Adson")
 
+equipes.append("-------")
+equipes.append("Nadyelly Vitória")
+equipes.append("Francisca Maria")
+equipes.append("Clara Vieira")
+equipes.append("-------")
 print(equipes)
